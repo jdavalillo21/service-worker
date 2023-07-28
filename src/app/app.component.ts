9b738051d24059b2fb53101f525c6e198bbe2776
Jemel <jemel.davalillo@gmail.com>
2023-07-28 19:47:58 -0400

    Navegar a error en caso de estar offline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-app';
+
+  constructor(
+    private router: Router
+  ){
+    if(!window.navigator.onLine){
+      this.router.navigate(['/error']);
+    }
+  }
 }
