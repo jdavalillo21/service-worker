5358d8cb0972e7bdbb9c06117c670fd0af5e6cd0
Jemel <jemel.davalillo@gmail.com>
2023-07-28 20:16:19 -0400

    agregando petición http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,46 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular-app';
   isOnline: boolean = navigator.onLine;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ){
     if(!window.navigator.onLine){
       this.router.navigate(['/error']);
     }
   }
+
+  ngOnInit(){
+    this.getDataFromServer();
+  }
+
+  getDataFromServer() {
+    const url = this.router.url;
+    this.http.get(url)
+      .pipe(
+        tap((data: any) => {
+          console.log('conectado', data);
+        }),
+        catchError((error: any) => {
+          this.router.navigate(['/error']);
+          return [];
+        })
+      )
+      .subscribe();
+  }
+
+
   @HostListener('window:offline', ['$event'])
   onOffline(event: Event) {
     this.isOnline = false;
